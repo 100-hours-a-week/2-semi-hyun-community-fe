fa43f07815dbb6cc7ba8297025d6d73a7eba52ea
HYUN <8135zxcvbn@gmail.com>
2024-11-19 18:06:25 +0900

    [Created] AddPost - AddPostFunction 추가 - 제목 maxLength 제한 - 버튼색 변경

---
+++ community/JS/DashBoard/AddPostFunction.js
@@ -0,0 +1,13 @@
+//제목, 본문 모두 입력 시 등록 버튼 활성화 ACA0EB > 7F6AEE
+const titleInput = document.getElementById('title'); // HTML 객체를 가리키는 객체
+const contextInput = document.getElementById('context');
+
+//제목 최대 26글자. 이후론 입력x
+titleInput.maxLength = 26; // HTML 속성으로 최대 길이 제한
+
+
+//버튼 색 변경
+//FIXME: 왜 또 동작이 안돼.
+if(titleInput.value.length > 0 && contextInput.value.length > 0){
+    document.querySelector('.create-button').style.backgroundColor = '#7F6AEE';
+}
