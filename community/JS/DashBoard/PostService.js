cf302774d9c8e1bec800f5cbd3970de83f6dac8e
HYUN <8135zxcvbn@gmail.com>
2024-11-21 14:51:18 +0900

    Refactor PostService.js to add name parameter in addPost function

--- community/JS/DashBoard/PostService.js
+++ community/JS/DashBoard/PostService.js
@@ -26,17 +26,19 @@ const getAllPosts = () => {
 
 
 //게시글 추가
-const addPost = ({ title, content, user_id, imageFilename = '' }) => {
+//NOTE: {} : 객체 구조 분해 -> 매개변수 순서가 자유롭다. 기본값 설정이 쉽다.
+const addPost = ({ title, content, name, user_id, imageFilename = '' }) => {
     console.log('addPost 호출');
     console.log(title, content, user_id, imageFilename);
 
     const posts = getAllPosts();
     
     const newPost = {
-        post_id: Date.now().toString(),
+        post_id: Date.now().toString(), //NOTE:현재시간을 밀리초 단위로 반환
         title,
         content,
         image: imageFilename,
+        name,
         user_id,
         created_date: new Date().toISOString(),
         updated_date: new Date().toISOString(),
@@ -52,10 +54,21 @@ const addPost = ({ title, content, user_id, imageFilename = '' }) => {
     return newPost;
 };
 
+
+// id로 게시글 조회
+
+const getPostById = (post_id) => {
+    const posts = getAllPosts();
+    const post = posts.find(post => post.post_id === post_id);
+    return post;
+
+};
+
 //게시글 수정
 
 
 
 module.exports = {
-    addPost
+    addPost,
+    getPostById
 }
