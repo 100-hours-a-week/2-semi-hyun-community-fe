0bd1f902638519073317bdb3a9c8b8720c2cbfc6
HYUN <8135zxcvbn@gmail.com>
2024-11-22 22:17:51 +0900

    [Update] dashboardRoute - add deletePost Route + Controller

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -10,7 +10,6 @@ router.get('/', authMiddleware, dashboardController.getDashboard);
 //게시글 목록조회 - 데이터 조회
 router.get('/data', authMiddleware, dashboardController.getDashboardData);
 
-
 //게시글 작성
 router.get('/write',authMiddleware,dashboardController.getWritePost);
 
@@ -26,6 +25,8 @@ router.get('/:post_id/data',authMiddleware,dashboardController.getPostData);
 //게시글 수정
 router.get('/:post_id/edit',authMiddleware,dashboardController.getEditPost);
 
+//게시글 삭제
+router.delete('/:post_id',authMiddleware,dashboardController.deletePost);
 
 //NOTE: 라우터 객체를 모듈로 내보내기 -> 주서버에 모두 작성하지 않아도됨
 module.exports = router;
