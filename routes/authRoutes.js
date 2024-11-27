3d9ff4037dc30c6d9f901360320ccb15928395f5
HYUN <8135zxcvbn@gmail.com>
2024-11-27 11:22:26 +0900

    [Update] load edit_user_info page _ 회원정보 수정 페이지 데이터 로드 - 회원정보 수정 페이지 html,css 수정

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,27 +4,21 @@ const express = require('express');
 // ㄴ라우터를 모듈화 하고 구조적으로 관리 가능
 const router = express.Router(); 
 const authController = require('../controllers/authController');
-const authEditController = require('../controllers/authEditController');
+
 
 //로그인 페이지를 클라이언트에 전송
 router.get('/login', authController.getLogin);
 //로그인 처리 (POST)
-router.post('/login', authController.postLogin);
+router.post('/login',authController.postLogin);
 
 //회원가입 페이지 라우트
-router.get('/signUp', authController.getSignUp);
+router.get('/signUp',authController.getSignUp);
 //회원가입 처리 라우트 (POST)
-router.post('/signUp', authController.postSignUp);
+router.post('/signUp',authController.postSignUp);
 
+//로그아웃 라우트
+// router.get('/logout', authController.getLogout);
 
-//회원정보 수정(닉네임) 페이지 라우트
-router.get('/users/:user_id/name', authEditController.getEditProfile);
-
-//회원정보 수정(비밀번호) 페이지 라우트
-router.get('/users/:user_id/password', authEditController.getEditPassword);
-
-//회원정보 삭제
-// router.delete('/user/:user_id',authEditController.deleteUser);
 
 //라우터 객체를 모듈로 내보내기
 //NOTE: 다른 파일에서 라우터를 쉽게 가져와 사용할 수 있다. + 재사용성
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,24 +1,25 @@
 const express = require('express');
 const router = express.Router(); 
+const authMiddleware = require('../community/JS/Middlewares/authMiddleware');
 const userController = require('../controllers/users/userController');
 
-//회원정보 수정(닉네임) 페이지 라우트
-router.get('/:user_id', userController.getEditProfile);
+//회원정보 수정(닉네임) 페이지 로드
+router.get('/me', authMiddleware, userController.getEditUser);
 
-//회원정보 수정 : 닉네임
-router.patch('/:user_id/name', userController.patchName);
+//회원정보 수정(닉네임) 데이터 조회
+router.get('/me/data', authMiddleware, userController.getEditUserData);
 
-//회원정보 수정 : 사진
-router.patch('/:user_id/profile', userController.patchProfile);
+//회원정보 수정 : 닉네임, 프로필 사진
+router.patch('/me/user_info', authMiddleware, userController.patchUserInfo);
 
 //회원정보 수정(비밀번호) 페이지 라우트
-router.get('/:user_id/password', userController.getEditPassword);
+router.get('/me/password', authMiddleware, userController.getEditPassword);
 
 //회원정보 수정 : 패스워드
-router.patch('/:user_id/password', userController.patchPassword);
+router.patch('/me/password', authMiddleware, userController.patchPassword);
 
 //회원정보 삭제
-router.delete('/:user_id',userController.deleteUser);
+router.delete('/me',authMiddleware, userController.deleteUser);
 
 //라우터 객체를 모듈로 내보내기
 //NOTE: 다른 파일에서 라우터를 쉽게 가져와 사용할 수 있다. + 재사용성
