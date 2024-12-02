d82e3872ed2f4f1114561868ca3d54b458e346a5
HYUN <8135zxcvbn@gmail.com>
2024-12-02 13:16:41 +0900

    [Update] patchPassword - 비밀번호 수정 기능 구현

--- community/JS/Users/features/UserService.js
+++ community/JS/Users/features/UserService.js
@@ -1,15 +1,20 @@
 const fs = require('fs');
 const path = require('path');
+const { patch } = require('../../../../routes/authRoutes');
 const rootDir = path.resolve(__dirname,'../','../','../');
 const userFilePath = path.join(rootDir,'data/login.json'); //../에서 .../로
 
 //json 저장 로직 : 모든 데이터를 로드 -> 특정 데이터 find 
 //-> 데이터 수정 -> 수정한 데이터로 덮기
 
-//데이터 저장
-const saveUsers = (users) => {
+//데이터 저장 -> promise로 return
+const saveUsers = async (users) => {
     try{
-        fs.writeFileSync(userFilePath, JSON.stringify(users,null,2),'utf8');
+        //NOTE fs
+        // 1. 콜백 기반 API     fs.writeFile(file,data,callback)
+        // 2. 동기식 API        fs.writeFileSync(file,data)
+        // 3. promise 기반 API  fs.promises.writeFile(file,data) 
+        await fs.promises.writeFile(userFilePath, JSON.stringify(users,null,2),'utf8');
         return true;
     }catch(error){
         console.error('Error writing user:',error);
@@ -45,7 +50,7 @@ const getUserById = (user_id) => {
 //사진 삭제
 
 //회원 정보 수정
-const patchPost = (user_id,userdata) => {
+const patchPost = async (user_id,userdata) => {
     const users = getAllUsers();
     const userIndex = users.findIndex(user => user.user_id === user_id);
 
@@ -62,13 +67,34 @@ const patchPost = (user_id,userdata) => {
     };
 
     //수정된 데이터를 저장
-    saveUsers(users);
+    await saveUsers(users);
 
     return true;
 }
 
+const patchPassword = async (user_id,password) => {
+    const users = getAllUsers();
+    const userIndex = users.findIndex(user => user.user_id === user_id);
+
+    if(userIndex === -1){
+        return null
+    }
+
+    //데이터 수정 (스프레드 연산자 사용)
+    users[userIndex] ={
+        ...users[userIndex],
+        password : password,
+        updated_date: new Date().toISOString()
+    };
+
+    //수정된 데이터를 저장
+    await saveUsers(users);
+
+    return true;
+}
 
 module.exports ={
     getUserById,
-    patchPost
+    patchPost,
+    patchPassword
 }
