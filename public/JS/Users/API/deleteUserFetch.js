41050fcf3336998e682e7d096a468cd74793c2e6
HYUN <8135zxcvbn@gmail.com>
2024-12-10 15:42:34 +0900

    [FIX] DeleteUser - 회원탈퇴 기능 추가 - 에러 :: 회원탈퇴 요청 완료 후 세션이 필요한 요청이 추가적으로 보내짐 -> 401 에러

--- public/JS/Users/API/deleteUserFetch.js
+++ public/JS/Users/API/deleteUserFetch.js
@@ -2,14 +2,15 @@ const deleteUser = document.getElementById('deleteUser');
 
 const showDelete = () => {
     if(confirm('정말 탈퇴하시겠습니까?')){
+
         deleteUserFetch();
     }
 }
 
 const deleteUserFetch = async () => {
-
+    
     try{
-        const response = await fetch('/users/me', {
+        const response = await fetch('http://localhost:3000/api/v1/users/me', {
             method: 'DELETE',
             credentials : 'include'
         });
@@ -17,28 +18,29 @@ const deleteUserFetch = async () => {
         const result = await response.json();
 
         if(!response.ok){
-
-            if(response.status === 404){
+            if ([404, 401, 403].includes(response.status)) {
                 alert(result.message);
                 return;
             }
-            else if(response.status === 401){
-                alert(result.message);
-                return;
-            }
-            else if(response.status === 403){
-                alert(result.message);
-                return;
-            }
-            else{
-                alert(result.message);
-                return;
-            }
+            alert(result.message);
+            return;
         }
 
         if(response.status === 200){
+
+            // 페이지 이동 전에 이벤트 리스너 제거
+            document.removeEventListener('DOMContentLoaded', getUserData);
+            document.removeEventListener('DOMContentLoaded', getHeaderImage);
+
             alert(result.message);
-            window.location.href = '/auth/login';
+            localStorage.clear();
+            sessionStorage.clear();
+
+            //NOTE: 브라우저 히스토리에서 현제 URL을 제거, 새 URL 삽입
+            // 뒤로가기 버튼을 눌러도 이전 페이지로 돌아갈 수 없다.
+            window.location.replace('/auth/login');
+
+            return;
         }
 
 
