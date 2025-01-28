200aaaca0337c6429b542891a9f2caac240e3b8b
HYUN <8135zxcvbn@gmail.com>
2025-01-28 23:15:31 +0900

    [Config] Update localhost to EC2 instance URL
    - Updated config.js and constants.js to replace localhost with EC2 instance IP
    - Modified CORS origin and BASE_URL to point to new server address

--- public/config/constants.js
+++ public/config/constants.js
@@ -1,5 +1,5 @@
 //공통으로 사용하는 상수 관리
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "http://3.35.218.81:3000";
 export const API_URL = `${BASE_URL}/api/v1`;
 
 export const urlUtils = {
